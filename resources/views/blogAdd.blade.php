118b7cbd449a2e4d709601c6169afc84cf888360
Pavol Bezek <bezek.palino@gmail.com>
2026-01-28 22:33:08 +0100

    e

--- resources/views/blogAdd.blade.php
+++ resources/views/blogAdd.blade.php
@@ -16,7 +16,7 @@
               <label for="title" class="form-label">Názov:</label>
               <input type="text" name="title" id="title" class="form-control" required>
             </div>
-
+            <div></div>
             <div class="mb-3">
               <label for="content" class="form-label">Obsah:</label>
               <textarea name="content" id="content" class="form-control" rows="5" required></textarea>
